51427217964adb501351e8d8e2797154c403a532
Gary LI <litao912002@gmail.com>
2013-04-10 10:29:02 -0500

    Trying online people

--- faye/client_event.rb
+++ faye/client_event.rb
@@ -1,12 +1,16 @@
 class ClientEvent
+    MNOITORED_CHANNELS = [ '/meta/subscribe', '/meta/disconnect']
     def incoming(message, callback)
-        puts message.inspect
+        return callback.call(message) unless MONITORED_CHANNEL.include? message['channel']
 
+        puts message.inspect
         callback.call(message)
+
     end
     def outgoing(message, callback)
-        puts message.inspect
-
         callback.call(message)
+    end
+    def faye_client
+        @faye_client ||= Faye::Client.new('http://localhosta:9292/faye')
     end
 end
