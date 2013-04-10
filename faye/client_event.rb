bf87f0d185a9b8d3cf2c469b303c71a8526f5568
FeiAline <yufei.flyingfish@gmail.com>
2013-04-11 07:02:22 +0800

    Now can report new connection and disconnet

--- faye/client_event.rb
+++ faye/client_event.rb
@@ -1,19 +1,25 @@
 class ClientEvent
     MONITORED_CHANNELS = [ '/meta/subscribe', '/meta/disconnect']
-    CHANNEL_BOARD_REGEX = /\/faye\/\d+/ # match pattern of /faye/num
+    CHANNEL_BOARD_REGEX = /(?<=\/faye\/)\d+/ # match pattern of /faye/num, negative lookbehind
+
 
     def incoming(message, callback)
         return callback.call(message) unless MONITORED_CHANNELS.include? message['channel']
 
         if MONITORED_CHANNELS[0].eql? message['channel']
-          puts "New subscription"
-          puts message.inspect
           if CHANNEL_BOARD_REGEX.match(message['subscription'])
-            puts "subscription:"+ message['subscription']
-            puts "id:"+message['id']
+            board_id = CHANNEL_BOARD_REGEX.match(message['subscription'])[0]
+            puts "publishing to board:"+board_id
+            faye_client.publish('/faye/status/'+board_id, {'message'=> 
+            {'status'=>'online','id'=>message['id'],'board_id'=>board_id}})
           end
         else
-          puts "Disconnect"+message['id']
+          if CHANNEL_BOARD_REGEX.match(message['subscription'])
+            board_id = CHANNEL_BOARD_REGEX.match(message['subscription'])[0]
+#            puts "board id: " + board_id
+            faye_client.publish('/faye/status/'+board_id, {'message'=> 
+            {'status'=>'disconnect','id'=>message['id'],'board_id'=>board_id}})
+          end
         end
         callback.call(message)
 
@@ -22,6 +28,6 @@ class ClientEvent
         callback.call(message)
     end
     def faye_client
-        @faye_client ||= Faye::Client.new('http://localhosta:9292/faye')
+        @faye_client ||= Faye::Client.new('http://localhost:9292/faye')
     end
 end
