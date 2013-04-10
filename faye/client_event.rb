788c8602cceab275676001fcafaa8c44c68d0aa7
FeiAline <yufei.flyingfish@gmail.com>
2013-04-11 03:46:00 +0800

    Successfully parsed the subscription information

--- faye/client_event.rb
+++ faye/client_event.rb
@@ -1,10 +1,20 @@
 class ClientEvent
     MONITORED_CHANNELS = [ '/meta/subscribe', '/meta/disconnect']
+    CHANNEL_BOARD_REGEX = /\/faye\/\d+/ # match pattern of /faye/num
 
     def incoming(message, callback)
         return callback.call(message) unless MONITORED_CHANNELS.include? message['channel']
 
-        puts message.inspect
+        if MONITORED_CHANNELS[0].eql? message['channel']
+          puts "New subscription"
+          puts message.inspect
+          if CHANNEL_BOARD_REGEX.match(message['subscription'])
+            puts "subscription:"+ message['subscription']
+            puts "id:"+message['id']
+          end
+        else
+          puts "Disconnect"+message['id']
+        end
         callback.call(message)
 
     end
